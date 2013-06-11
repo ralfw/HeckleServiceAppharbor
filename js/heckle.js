002d49baea3a3d56dbca806d92586eb72f8a0fb1
Ralf Westphal <info@ralfw.de>
2013-06-11 11:00:44 +0200

    Übernahme Icenium Sourcen

---
+++ js/heckle.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+// Wait for PhoneGap to load
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is ready
+function onDeviceReady() {
+    navigator.splashscreen.hide();
+}
+
+$(document).ready(function(){
+    Integrieren();
+});
+
+
